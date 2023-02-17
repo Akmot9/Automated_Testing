ed5005b16cbf9a8489ab6c5cc2df295f16854162
akmot <avicocyprien@yahoo.com>
2023-02-17 16:23:15 +0100

    the script.sh need a humain !

--- test.sh
+++ test.sh
@@ -1,12 +1,38 @@
 #!/bin/bash
 
-# Wait for 20 seconds
-sleep 10
-
 # Get the current date and time
 now=$(date +"%Y-%m-%d %H:%M:%S")
 
-# Write the date to three different files
-echo $now > file1.csv
-echo $now > file2.json
-echo $now > file3.log
+printf "Voulez vous faire un :\n    1.  relevé json  \n    2.  relevé de csv \n    3.  Les deux\n"
+  printf "Choisissez 1/2/3\n"
+  read Y
+  if [ $Y -eq 1 ]
+  then
+    # Wait for 20 seconds
+    sleep 10
+    echo $now > file2.json
+    echo $now > file3.log
+    #printf $z
+  elif [ $Y -eq 2 ]
+  then
+    # Wait for 20 seconds
+    sleep 10
+    echo $now > file1.csv
+    echo $now > file3.log
+    #printf $z
+  else
+    # Wait for 20 seconds
+    sleep 10
+    # Write the date to three different files
+    echo $now > file1.csv
+    echo $now > file2.json
+    echo $now > file3.log
+    #printf $z
+  fi
+
+
+
+
+
+
+
