63cffdccfef5a5c96bf77ba1c0f42e7ec29cc916
akmot <avicocyprien@yahoo.com>
2023-02-17 11:28:21 +0100

    less bugged

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Wait for 20 seconds
-sleep 20
+sleep 10
 
 # Get the current date and time
 now=$(date +"%Y-%m-%d %H:%M:%S")
 
 # Write the date to three different files
-echo $now > file1.txt
-echo $now > file2.txt
-echo $now > file3.txt
+echo $now > file1.csv
+echo $now > file2.json
+echo $now > file3.log
--- test2.py
+++ test2.py
@@ -1,6 +1,7 @@
 import paramiko
 import os
 import scp
+import time
 
 # Paramètres de connexion au serveur distant
 host = '192.168.153.150'
@@ -10,12 +11,11 @@ password = 'admin'
 
 # Répertoire local contenant le logiciel à copier
 cwd = os.getcwd()
-local_path = cwd+'\\test.sh'
+local_path = os.path.join(cwd, 'test.sh')
 print(local_path)
 
 # Répertoire distant où copier le logiciel
 remote_path = 'Téléchargements/test'
-
 
 # Connexion au serveur distant
 ssh = paramiko.SSHClient()
@@ -24,8 +24,8 @@ ssh.connect(host, port=port, username=username, password=password)
 
 # Création du dossier "test" dans le répertoire distant
 command = 'mkdir ~/Téléchargements/test'
+test_folder= '~/Téléchargements/test'
 stdin, stdout, stderr = ssh.exec_command(command)
-
 
 # Copie du logiciel sur le serveur distant
 with scp.SCPClient(ssh.get_transport()) as scp_client:
@@ -37,4 +37,23 @@ stdin, stdout, stderr = ssh.exec_command(command)
 
 # Exécution du logiciel sur le serveur distant
 stdin, stdout, stderr = ssh.exec_command('cd ' + remote_path + ' && ./test.sh')
+print('Executing...')
 
+# Check if the script is still running
+while not stdout.channel.exit_status_ready():
+    try:
+        output = stdout.read().decode()
+        print(output)
+        time.sleep(1)
+    except paramiko.SSHException:
+        print("Error occurred while reading SSH output.")
+        break
+
+# Copie des fichiers depuis le serveur distant vers le répertoire local
+with scp.SCPClient(ssh.get_transport()) as scp_client:
+    scp_client.get(test_folder, local_path, recursive=True)
+
+print("Finished")
+
+# Fermeture de la connexion SSH
+ssh.close()
