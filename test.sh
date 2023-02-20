19e5cf884a0cda2ea8d458baaeea87308e704de6
akmot <avicocyprien@yahoo.com>
2023-02-20 16:54:38 +0100

    script to be tasted

--- test.sh
+++ test.sh
@@ -3,36 +3,59 @@
 # Get the current date and time
 now=$(date +"%Y-%m-%d %H:%M:%S")
 
-printf "Voulez vous faire un :\n    1.  relevé json  \n    2.  relevé de csv \n    3.  Les deux\n"
-  printf "Choisissez 1/2/3\n"
-  read Y
-  if [ $Y -eq 1 ]
-  then
-    # Wait for 20 seconds
-    sleep 10
-    echo $now > file2.json
-    echo $now > file3.log
-    #printf $z
-  elif [ $Y -eq 2 ]
-  then
-    # Wait for 20 seconds
-    sleep 10
-    echo $now > file1.csv
-    echo $now > file3.log
-    #printf $z
-  else
-    # Wait for 20 seconds
-    sleep 10
-    # Write the date to three different files
-    echo $now > file1.csv
-    echo $now > file2.json
-    echo $now > file3.log
-    #printf $z
-  fi
+i=$(uname -a)
+echo "now : $now"
+echo "i : $i"
 
+# Prompt the user for the option to choose
+printf "Voulez vous faire un :\n    1. relevé date\n    2. relevé infos\n    3. Les deux\nChoisissez 1/2/3: "
+read option
 
+# Prompt the user for the second option to choose
+printf "Voulez vous faire un :\n    1. relevé json\n    2. relevé de csv\n    3. Les deux\nChoisissez 1/2/3: "
+read option2
 
+# Prompt the user for three letters
+read -p "Entrez trois lettres: " lettres
 
+# Prompt the user for two letters
+read -p "Entrez deux lettres: " lettres2
 
+# Prompt the user for six numbers
+read -p "Entrez six chiffres: " chiffres
 
+echo "Vous avez choisi l'option $option et l'option $option2."
+echo "Les lettres que vous avez entrées sont $lettres et $lettres2."
+echo "Les chiffres que vous avez entrés sont $chiffres."
 
+name="${lettres}_${lettres2}_${chiffres}"
+
+if [ $option -eq 1 ]
+then
+  infos="${now}"
+elif [ $option -eq 2 ]
+then
+  infos="${i} ${lettres} ${lettres2} ${chiffres}"
+else
+  infos="${i} ${now} ${lettres} ${lettres2} ${chiffres}"
+fi
+
+if [ $option2 -eq 1 ]
+then
+  # Wait for 10 seconds
+  sleep 10
+  echo $infos > $name.json
+  echo $infos > $name.log
+elif [ $option2 -eq 2 ]
+then
+  # Wait for 10 seconds
+  sleep 10
+  echo $infos > $name.csv
+  echo $infos > $name.log
+else
+  # Wait for 10 seconds
+  sleep 10
+  echo $infos > $name.csv
+  echo $infos > $name.json
+  echo $infos > $name.log
+fi
